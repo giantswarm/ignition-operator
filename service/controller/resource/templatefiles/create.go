c0966e7168cdfd2310844a54e348d154599c42b6
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-12-18 23:19:58 +0100

    Add goldenfile test

--- service/controller/resource/templatefiles/create.go
+++ service/controller/resource/templatefiles/create.go
@@ -1,9 +1,67 @@
 package templatefiles
 
 import (
+	"bytes"
 	"context"
+	"html/template"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/shurcooL/httpfs/vfsutil"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/ignition-operator/data"
+	"github.com/giantswarm/ignition-operator/service/controller/controllercontext"
+)
+
+const (
+	filesdir = "/files"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	cc.Status.Files, err = renderFiles(cc.Spec)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
+}
+
+func renderFiles(spec controllercontext.ContextSpec) (map[string]string, error) {
+	files := make(map[string]string)
+
+	err := vfsutil.WalkFiles(data.Assets, filesdir, func(path string, f os.FileInfo, rs io.ReadSeeker, err error) error {
+		if f.Mode().IsRegular() {
+			file, err := vfsutil.ReadFile(data.Assets, path)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			tmpl, err := template.New(path).Parse(string(file))
+			if err != nil {
+				return microerror.Maskf(err, "failed to parse file %#q", path)
+			}
+			var data bytes.Buffer
+			tmpl.Execute(&data, spec)
+
+			relativePath, err := filepath.Rel(filesdir, path)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+			files[relativePath] = string(data.Bytes())
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return files, nil
 }
---
+++ service/controller/resource/templatefiles/create_test.go
@@ -0,0 +1,75 @@
+package templatefiles
+
+import (
+	"bytes"
+	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/giantswarm/ignition-operator/service/controller/controllercontext"
+	"github.com/giantswarm/ignition-operator/service/controller/internal/unittest"
+)
+
+var update = flag.Bool("update", false, "update .golden CF template file")
+
+// Test_Controller_Resource_TCNP_Template_Render tests tenant cluster
+// CloudFormation template rendering. It is meant to be used as a tool to easily
+// check resulting CF template and prevent from accidental CF template changes.
+//
+// It uses golden file as reference template and when changes to template are
+// intentional, they can be updated by providing -update flag for go test.
+//
+//  go test ./service/controller/resource/templatefiles -run Test_Controller_Resource_Templatefiles_Renderfiles -update
+//
+func Test_Controller_Resource_Templatefiles_Renderfiles(t *testing.T) {
+	testCases := []struct {
+		name   string
+		ccSpec controllercontext.ContextSpec
+	}{
+		{
+			name: "case 0: basic test",
+			ccSpec: controllercontext.ContextSpec{
+				BaseDomain: "someshit",
+				Etcd: controllercontext.ContextSpecEtcd{
+					Domain: "abcd",
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			f, err := renderFiles(tc.ccSpec)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			yamlFiles, err := yaml.Marshal(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			p := filepath.Join("testdata", unittest.NormalizeFileName(tc.name)+".golden")
+
+			if *update {
+				err := ioutil.WriteFile(p, yamlFiles, 0644)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			goldenFile, err := ioutil.ReadFile(p)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !bytes.Equal(yamlFiles, goldenFile) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(string(goldenFile), string(yamlFiles)))
+			}
+		})
+	}
+}
