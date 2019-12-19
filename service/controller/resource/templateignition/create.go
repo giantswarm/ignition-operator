12925d643c15019a4eb77a94c95dde1eabebd8ac
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-12-19 23:24:09 +0100

    Add output configmap and worker rendering

--- service/controller/resource/templateignition/create.go
+++ service/controller/resource/templateignition/create.go
@@ -4,19 +4,53 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/ignition-operator/service/controller/controllercontext"
 	"github.com/giantswarm/ignition-operator/service/controller/key"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	cr, err := key.ToIgnition(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
 	cc, err := controllercontext.FromContext(ctx)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	cc.Status.Units, err = key.Render(cc.Status, key.UnitPath, false)
+	master, err := key.Render(cc, key.MasterTemplatePath, false)
 	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	worker, err := key.Render(cc, key.WorkerTemplatePath, false)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.StatusConfigMapName(cr.Spec.ClusterID),
+			Namespace: key.DefaultNamespace,
+		},
+		Data: map[string]string{
+			"master": master["."],
+			"worker": worker["."],
+		},
+	}
+
+	_, err = r.k8sClient.K8sClient().CoreV1().ConfigMaps("giantswarm").Update(cm)
+	if apierrors.IsNotFound(err) {
+		r.k8sClient.K8sClient().CoreV1().ConfigMaps("giantswarm").Create(cm)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+	} else if err != nil {
 		return microerror.Mask(err)
 	}
 
